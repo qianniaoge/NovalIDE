bd842616ac670c92a0d552b686e6e72c2b556e41
wekay <wekay102200@sohu.com>
2017-12-29 15:38:35 +0800

    add makedirs function

--- noval/util/sysutils.py
+++ noval/util/sysutils.py
@@ -118,3 +118,19 @@ systemStartTime = getCurrentTimeAsFloat()
 
 def CopyToClipboard(str):
     pyperclip.copy(str)
+
+def MakeDirs(dirname):
+
+    dirname = os.path.abspath(dirname)
+    dirname = dirname.replace("\\","/")
+    dirnames = dirname.split("/")
+    destdir = ""
+    destdir = os.path.join(dirnames[0] + "/",dirnames[1])
+    
+    if not os.path.exists(destdir):
+        os.mkdir(destdir)
+        
+    for name in dirnames[2:]:
+        destdir=os.path.join(destdir,name)
+        if not os.path.exists(destdir):
+            os.mkdir(destdir)
