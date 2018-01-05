98eb142b977916d952b142699ab9cc31180c563c
wekay <kan.wu@genetalks.com>
2018-01-05 19:41:57 +0800

    添加自动创建多层文件夹函数

---
+++ noval/parser/utils.py
@@ -0,0 +1,16 @@
+import os
+
+def MakeDirs(dirname):
+    dirname = os.path.abspath(dirname)
+    dirname = dirname.replace("\\","/")
+    dirnames = dirname.split("/")
+    destdir = ""
+    destdir = os.path.join(dirnames[0] + "/",dirnames[1])
+    
+    if not os.path.exists(destdir):
+        os.mkdir(destdir)
+        
+    for name in dirnames[2:]:
+        destdir=os.path.join(destdir,name)
+        if not os.path.exists(destdir):
+            os.mkdir(destdir)
